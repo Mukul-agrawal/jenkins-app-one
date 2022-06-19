93f032173b0f995dc2ac9dccfb1a14286eff9fd5
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2022-06-19 11:20:32 +0530

    created maven project

---
+++ jenins-app-one/src/main/java/com/wipro/jenkins/App.java
@@ -0,0 +1,16 @@
+package com.wipro.jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static String sayHello() {
+		return "Hello Jenkins!";
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( sayHello() );
+    }
+}
